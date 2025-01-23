3346c1f30acd60f08a9cc27c7b994baeb94ab6c7
srh-sloan <sarah.sloan@communities.gov.uk>
2025-01-23 14:55:40 +0000

    Replacing old landing page route

--- apply/routes.py
+++ apply/routes.py
@@ -6,7 +6,7 @@ from pre_award.common.blueprints import Blueprint
 apply_bp = Blueprint("apply_routes", __name__, template_folder="templates")
 
 
-@apply_bp.route("/monolith/<fund_short_name>/<round_short_name>")
+@apply_bp.route("/funding-round/<fund_short_name>/<round_short_name>")
 def landing_page(fund_short_name: str, round_short_name: str):
     fund, round = get_fund_and_round(fund_short_name, round_short_name)
     if not fund or not round:
