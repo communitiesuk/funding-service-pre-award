f4c33f3b9434f601431ffd25de04f8c74c680632
srh-sloan <sarah.sloan@communities.gov.uk>
2025-01-27 11:00:58 +0000

    review tweaks

--- apply/routes.py
+++ apply/routes.py
@@ -1,4 +1,4 @@
-from flask import abort, current_app, render_template
+from flask import abort, render_template
 
 from data.crud.fund_round_queries import get_round
 from pre_award.common.blueprints import Blueprint
@@ -8,10 +8,6 @@ apply_bp = Blueprint("apply_routes", __name__, template_folder="templates")
 
 @apply_bp.route("/funding-round/<fund_short_name>/<round_short_name>")
 def landing_page(fund_short_name: str, round_short_name: str):
-    current_app.logger.info(
-        "Apply landing page for fund %(fund_short_name)s, round %(round_short_name)s",
-        dict(fund_short_name=fund_short_name, round_short_name=round_short_name),
-    )
     round = get_round(fund_short_name, round_short_name)
     if not round:
         return abort(404)
