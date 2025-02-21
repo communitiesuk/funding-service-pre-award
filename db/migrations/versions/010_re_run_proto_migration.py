cb5255cfa5e7135a216486667823e76f63eec69c
Steven <steven.fountain@communities.gov.uk>
2025-02-21 17:32:55 +0000

    PROTO: add number question type
    Adds a number question type that appropriately sets the `numeric`
    inputmode for the design system text field.

--- db/migrations/versions/010_re_run_proto_migration.py
+++ db/migrations/versions/010_re_run_proto_migration.py
@@ -19,7 +19,9 @@ depends_on = None
 
 fundstatus_enum = postgresql.ENUM("DRAFT", "LIVE", "RETIRED", name="fundstatus", create_type=False)
 templatetype_enum = postgresql.ENUM("APPLICATION", "REPORTING", name="templatetype", create_type=False)
-questiontype_enum = postgresql.ENUM("TEXT_INPUT", "TEXTAREA", "RADIOS", name="questiontype", create_type=False)
+questiontype_enum = postgresql.ENUM(
+    "TEXT_INPUT", "TEXTAREA", "RADIOS", "NUMBER", name="questiontype", create_type=False
+)
 
 
 def upgrade():
