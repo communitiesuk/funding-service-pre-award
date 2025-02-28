d82605743d13aab2b40468d8176bc0a489add383
Samuel Williams <samuel.williams@communities.gov.uk>
2025-02-28 09:30:02 +0000

    tweak slug generation

--- proto/common/helpers.py
+++ proto/common/helpers.py
@@ -5,4 +5,4 @@ def make_url_slug(sentence):
     no_apostrophes = re.sub(r"""['"]""", "", sentence)
     normalised_sentence = re.sub(r"[^A-Za-z0-9]+", "-", no_apostrophes)
     slug = re.sub(r"-+", "-", normalised_sentence)
-    return slug
+    return slug.lower().strip("-")
