926d6fc8d294f799335938d4f669daa8c21b5455
Samuel Williams <samuel.williams@communities.gov.uk>
2025-02-28 09:49:17 +0000

    restore short circuit logic for conditional questions
    otherwise the list of questions that need to be answered errors out

--- proto/form_runner/form_route_helper.py
+++ proto/form_runner/form_route_helper.py
@@ -8,6 +8,7 @@ from proto.common.data.models.data_collection import (
     ProtoDataCollectionQuestionCondition,
 )
 from proto.form_runner.expressions import build_context_evaluator
+from proto.form_runner.helpers import get_answer_value_for_question_from_section_data
 
 
 def get_visible_questions_for_section_instance(
@@ -41,6 +42,18 @@ def get_visible_questions_for_section_instance(
 def evaluate_condition(
     all_section_data: list[ProtoDataCollectionInstanceSectionData], condition: ProtoDataCollectionQuestionCondition
 ) -> bool:
+    if condition.depends_on_question:
+        section_data = next(
+            (sd for sd in all_section_data if sd.section_id == condition.depends_on_question.section_id), None
+        )
+        if not section_data:  # If that section hasn't yet been completed
+            return False
+
+        depends_on_answer_text = get_answer_value_for_question_from_section_data(
+            section_data=section_data, question=condition.depends_on_question
+        )
+        if depends_on_answer_text is None:
+            return False
     context_evaluator = build_context_evaluator(this_collection=all_section_data[0].instance, answer=None)
     return bool(context_evaluator(condition.expression))
 
