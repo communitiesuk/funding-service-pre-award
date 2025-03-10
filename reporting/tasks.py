807aa7f943a4b65eb9799c268b4d732283e6d68c
Albert Artur Kolozsvari <albertartk@gmail.com>
2025-03-10 21:18:48 +0000

    Add email

--- reporting/tasks.py
+++ reporting/tasks.py
@@ -1,9 +1,12 @@
+import io
+
 from flask import current_app
 from invoke import Context, task  # type: ignore[attr-defined]
 from openpyxl import Workbook
 
 from app import create_app
 from reporting.reporting import export_applicant_information, export_end_of_application_survey_data
+from services.notify import NotificationError, get_notification_service
 
 
 def build_report_impl() -> None:
@@ -25,7 +28,15 @@ def build_report_impl() -> None:
     for report_function in reports:
         report_function(workbook)
 
-    workbook.save("reports.xlsx")
+    output = io.BytesIO()
+    workbook.save(output)
+    output.seek(0)
+
+    try:
+        notification = get_notification_service().send_report_email(file_buffer=output)
+        current_app.logger.info("Report email sent %(notification_id)s", dict(notification_id=notification.id))
+    except NotificationError:
+        current_app.logger.exception("Error sending report email")
 
     current_app.logger.info("Building the report finished")
 
