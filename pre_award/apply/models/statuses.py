ffe65eb7052cabc6e91e5e113c3d469ab5b66447
Albert Artur Kolozsvari <albertartk@gmail.com>
2025-04-04 13:30:56 +0100

    Adjust pre-award tags

--- pre_award/apply/models/statuses.py
+++ pre_award/apply/models/statuses.py
@@ -3,12 +3,12 @@ from flask_babel import gettext
 
 def get_formatted(value: str):
     statuses = {
-        "NOT_STARTED": gettext("Not Started"),
-        "IN_PROGRESS": gettext("In Progress"),
+        "NOT_STARTED": gettext("Not started"),
+        "IN_PROGRESS": gettext("In progress"),
         "COMPLETED": gettext("Completed"),
         "SUBMITTED": gettext("Submitted"),
-        "NOT_SUBMITTED": gettext("Not Submitted"),
-        "READY_TO_SUBMIT": gettext("Ready to Submit"),
-        "CHANGE_REQUESTED": "Change Requested",
+        "NOT_SUBMITTED": gettext("Not submitted"),
+        "READY_TO_SUBMIT": gettext("Ready to submit"),
+        "CHANGE_REQUESTED": "Change requested",
     }
     return statuses.get(value, value.replace("_", " ").strip().title())
