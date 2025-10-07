ef2d496f3d66fcad4617296cf380efc82b3ada95
Zoltan Szabo <38726653+sc15zs@users.noreply.github.com>
2025-10-07 19:35:57 +0100

    Adding All questions, py config file, scored and unscored sections

---
+++ pre_award/assessment_store/config/mappings/shif_mapping_parts/apply_scored_sections.py
@@ -0,0 +1,258 @@
+# ruff: noqa: E501
+
+scored_sections = [
+    {
+        "id": "co_production_with_tenants",
+        "weighting": 0.2,
+        "name": "Co-production with tenants",
+        "sub_criteria": [
+            {
+                "id": "co_production_with_tenants",
+                "name": "Co-production with tenants",
+                "themes": [
+                    {
+                        "id": "co_production_with_tenants",
+                        "name": "Co-production with tenants",
+                        "answers": [
+                            {
+                                "field_id": "CjawNO",
+                                "form_name": "shif-apply-co-production-with-tenants",
+                                "field_type": "freeTextField",
+                                "presentation_type": "text",
+                                "question": "How, and to what extent, have tenants been involved in co-producing your proposal",
+                            },
+                        ],
+                    }
+                ],
+            },
+        ],
+    },
+    {
+        "id": "costs_value_for_money_and_risk_management",
+        "weighting": 0.05,
+        "name": "Costs, value for money and risk management",
+        "sub_criteria": [
+            {
+                "id": "costs_value_for_money_and_risk_management",
+                "name": "Costs, value for money and risk management",
+                "themes": [
+                    {
+                        "id": "costs_value_for_money_and_risk_management",
+                        "name": "Costs, value for money and risk management",
+                        "answers": [
+                            {
+                                "field_id": "BwHiiN",
+                                "form_name": "shif-apply-costs-and-value-for-money",
+                                "field_type": "yesNoField",
+                                "presentation_type": "text",
+                                "question": "Are you seeking funding to cover the total cost of the project",
+                            },
+                            {
+                                "field_id": "UoneMB",
+                                "form_name": "shif-apply-costs-and-value-for-money",
+                                "field_type": "numberField",
+                                "presentation_type": "text",
+                                "question": "Total funding requested",
+                            },
+                            {
+                                "field_id": "vltUZa",
+                                "form_name": "shif-apply-costs-and-value-for-money",
+                                "field_type": "numberField",
+                                "presentation_type": "text",
+                                "question": "Total cost of project",
+                            },
+                            {
+                                "field_id": "LBPcsu",
+                                "form_name": "shif-apply-costs-and-value-for-money",
+                                "field_type": "freeTextField",
+                                "presentation_type": "text",
+                                "question": "How will you fund the outstanding costs of the project?",
+                            },
+                            {
+                                "field_id": "OdMILX",
+                                "form_name": "shif-apply-costs-and-value-for-money",
+                                "field_type": "numberField",
+                                "presentation_type": "text",
+                                "question": "How much funding are you seeking for your project",
+                            },
+                            {
+                                "field_id": "eeNMwb",
+                                "form_name": "shif-apply-costs-and-value-for-money",
+                                "field_type": "yesNoField",
+                                "presentation_type": "text",
+                                "question": "Does this include non-recoverable VAT costs?",
+                            },
+                            {
+                                "field_id": "bKXFmG",
+                                "form_name": "shif-apply-costs-and-value-for-money",
+                                "field_type": "freeTextField",
+                                "presentation_type": "text",
+                                "question": "Describe your project budget and assumptions",
+                            },
+                            {
+                                "field_id": "KVZvBK",
+                                "form_name": "shif-apply-costs-and-value-for-money",
+                                "field_type": "clientSideFileUploadField",
+                                "presentation_type": "text",
+                                "question": "Upload your project budget with a breakdown of the key costs of your project",
+                            },
+                            {
+                                "field_id": "gRddtf",
+                                "form_name": "shif-apply-costs-and-value-for-money",
+                                "field_type": "freeTextField",
+                                "presentation_type": "text",
+                                "question": "How will your project deliver value for money?",
+                            },
+                        ],
+                    }
+                ],
+            },
+        ],
+    },
+    {
+        "id": "innovation_and_scalability",
+        "weighting": 0.2,
+        "name": "Innovation and scalability",
+        "sub_criteria": [
+            {
+                "id": "innovation_and_scalability",
+                "name": "Innovation and scalability",
+                "themes": [
+                    {
+                        "id": "innovation_and_scalability",
+                        "name": "Innovation and scalability",
+                        "answers": [
+                            {
+                                "field_id": "WJTBzi",
+                                "form_name": "shif-apply-innovation-and-scalability",
+                                "field_type": "freeTextField",
+                                "presentation_type": "text",
+                                "question": "How is the proposed project innovative?",
+                            },
+                            {
+                                "field_id": "czkAFo",
+                                "form_name": "shif-apply-innovation-and-scalability",
+                                "field_type": "freeTextField",
+                                "presentation_type": "text",
+                                "question": "How does this project have the potential to be scaled up or replicated on a wider scale?",
+                            },
+                        ],
+                    }
+                ],
+            },
+        ],
+    },
+    {
+        "id": "outcomes_and_measuring_impact",
+        "weighting": 0.15,
+        "name": "Outcomes and measuring impact",
+        "sub_criteria": [
+            {
+                "id": "outcomes_and_measuring_impact",
+                "name": "Outcomes and measuring impact",
+                "themes": [
+                    {
+                        "id": "outcomes_and_measuring_impact",
+                        "name": "Outcomes and measuring impact",
+                        "answers": [
+                            {
+                                "field_id": "PHcUVY",
+                                "form_name": "shif-apply-outcomes-and-measuring-impact",
+                                "field_type": "freeTextField",
+                                "presentation_type": "text",
+                                "question": "Describe the positive impacts your project is expected to have",
+                            },
+                            {
+                                "field_id": "oRkpsw",
+                                "form_name": "shif-apply-outcomes-and-measuring-impact",
+                                "field_type": "freeTextField",
+                                "presentation_type": "text",
+                                "question": "What key performance indicators (KPIs) you will use to monitor the success of your project?",
+                            },
+                        ],
+                    }
+                ],
+            },
+        ],
+    },
+    {
+        "id": "risk_management",
+        "weighting": 0.05,
+        "name": "Risk management",
+        "sub_criteria": [
+            {
+                "id": "risk_management",
+                "name": "Risk management",
+                "themes": [
+                    {
+                        "id": "risk_management",
+                        "name": "Risk management",
+                        "answers": [
+                            {
+                                "field_id": "WkOXbJ",
+                                "form_name": "shif-apply-risk-management",
+                                "field_type": "freeTextField",
+                                "presentation_type": "text",
+                                "question": "What risks have you identified, and how will you manage and mitigate these risks",
+                            },
+                        ],
+                    }
+                ],
+            },
+        ],
+    },
+    {
+        "id": "your_proposal",
+        "weighting": 0.35,
+        "name": "Your proposal",
+        "sub_criteria": [
+            {
+                "id": "your_proposal",
+                "name": "Your proposal",
+                "themes": [
+                    {
+                        "id": "your_proposal",
+                        "name": "Your proposal",
+                        "answers": [
+                            {
+                                "field_id": "lTFJnY",
+                                "form_name": "shif-apply-your-proposal",
+                                "field_type": "freeTextField",
+                                "presentation_type": "text",
+                                "question": "What challenges are you trying to address",
+                            },
+                            {
+                                "field_id": "FFNuZw",
+                                "form_name": "shif-apply-your-proposal",
+                                "field_type": "freeTextField",
+                                "presentation_type": "text",
+                                "question": "How will the proposal address the challenges specified in your problem statement",
+                            },
+                            {
+                                "field_id": "PdxtmT",
+                                "form_name": "shif-apply-your-proposal",
+                                "field_type": "clientSideFileUploadField",
+                                "presentation_type": "text",
+                                "question": "Upload your project plan",
+                            },
+                            {
+                                "field_id": "Fxpsiw",
+                                "form_name": "shif-apply-your-proposal",
+                                "field_type": "freeTextField",
+                                "presentation_type": "text",
+                                "question": "Who will be involved in implementing the proposal",
+                            },
+                            {
+                                "field_id": "RlFFTW",
+                                "form_name": "shif-apply-your-proposal",
+                                "field_type": "textField",
+                                "presentation_type": "text",
+                                "question": "What is the name of your project",
+                            },
+                        ],
+                    }
+                ],
+            },
+        ],
+    },
+]
